85ba5c34773553763cbf837878063d19f320ee86
Maks Kolman <kolman.maks@gmail.com>
2015-07-11 23:40:10 +0200

    Added ids to api

--- hmh/serializers.py
+++ hmh/serializers.py
@@ -6,34 +6,34 @@ import hmhmod.models as models
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ('url', 'name')
+        fields = ('id', 'url', 'name')
 
 
 class CandidateSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Candidate
-        fields = ('url', 'first_name', 'last_name', 'party')
+        fields = ('id', 'url', 'first_name', 'last_name', 'party')
 
 
 class PartySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Party
-        fields = ('url', 'name')
+        fields = ('id', 'url', 'name')
 
 
 class IssueSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Issue
-        fields = ('url', 'short_desc', 'description')
+        fields = ('id', 'url', 'short_desc', 'description')
 
 
 class OpinionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Opinion
-        fields = ('url', 'candidate', 'issue', 'value')
+        fields = ('id', 'url', 'candidate', 'issue', 'value')
