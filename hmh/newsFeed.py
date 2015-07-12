3adea639ad484003d0d77504cb25740721fd517d
goskas <goska.science@gmail.com>
2015-07-12 11:17:09 +0200

    Removing datetime data from news.

--- hmh/newsFeed.py
+++ hmh/newsFeed.py
@@ -3,6 +3,18 @@ import urllib.parse as urlencoder
 import json
 
 
+def remove_datetime_data(text):
+    tmp = text
+    if '<b>...</b>' in tmp[0:50]:
+        tmp = tmp.split('<b>...</b>')
+        if len(tmp) != 2:
+            tmp.pop(0)
+            tmp = '...'.join(tmp)
+        else:
+            tmp = tmp[1]
+    return tmp.strip()
+
+
 def getFeed(query):
     query = urlencoder.urlencode({'q': query})
     r = urllib.urlopen('https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&'+ query)
@@ -13,6 +25,7 @@ def getFeed(query):
     i = 0
 
     for item in js:
+        #item['contentSnippet'] = remove_datetime_data(item['contentSnippet'])
         megaArray += [{'title': item['title'], 'link': item['link'], 'snippet': item['contentSnippet']}]
         # megaArray += [item['link']]
         i += 1
@@ -25,3 +38,8 @@ def getFeed(query):
 
 
 ##Return first 5 titles and links to recent news about query
+if __name__ == '__main__':
+    for i in getFeed('Hillary'):
+         tmp = i['snippet']
+         #tmp = 'Nekoc nekje <b>...</b> probamo <b>...</b> in <b>...</b>'
+         print(tmp)
